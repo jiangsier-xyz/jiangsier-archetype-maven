1c9069ba66a81f37675b3dc94137846333053226
jiangsier <leiyi.jp@gmail.com>
2024-06-12 22:40:04 +0800

    Adjust CommonUtils.java

--- src/main/resources/archetype-resources/__rootArtifactId__-start/src/main/java/api/util/CommonUtils.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-start/src/main/java/api/util/CommonUtils.java
@@ -12,17 +12,14 @@ import java.util.Objects;
 @Slf4j
 public class CommonUtils {
     public static <S, T> T convert(S source, Class<T> clazz) {
-        if (Objects.isNull(source)) {
-            return null;
-        }
-
         try {
+            Objects.requireNonNull(source);
             T response = clazz.getDeclaredConstructor().newInstance();
             BeanUtils.copyProperties(source, response);
             return response;
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             log.error("Failed to construct instance of {}", clazz.getSimpleName(), e);
-        }
-        return null;
+            throw new RuntimeException(e);
+        };
     }
 }
