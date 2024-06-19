d9b3021b46fa6da6ca0cebd1b811bde19c873362
jiangsier <leiyi.jp@gmail.com>
2024-06-19 14:40:53 +0800

    Fix AuthUtils Issue

--- src/main/resources/archetype-resources/__rootArtifactId__-start/src/main/java/api/util/AuthUtils.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-start/src/main/java/api/util/AuthUtils.java
@@ -14,7 +14,7 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Objects;
 import java.util.Optional;
