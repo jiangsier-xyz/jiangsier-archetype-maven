8ba2c75151396a314f910e346637c0461103e13c
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2024-12-10 10:51:59 +0800

    feat: support checking file path

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-common/src/main/java/util/SecurityUtils.java
@@ -0,0 +1,39 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.AccessDeniedException;
+
+public class SecurityUtils {
+    private static final Logger log = LoggerFactory.getLogger(HttpUtils.class);
+
+    public static String filterPath(String path) throws AccessDeniedException {
+        if (StringUtils.isEmpty(path)) {
+            return path;
+        }
+
+        String temp = path;
+        while (temp.indexOf('%') != -1) {
+            try {
+                temp = URLDecoder.decode(temp, StandardCharsets.UTF_8);
+            } catch (Exception e) {
+                log.warn("Invalid path: {}", path, e);
+                throw new IllegalArgumentException("Invalid path: " + path);
+            }
+        }
+
+        if (temp.contains("..") || temp.charAt(0) == '/') {
+            log.warn("Invalid path: {}", path);
+            throw new AccessDeniedException("Invalid path: " + path);
+        }
+
+        return path;
+    }
+}
