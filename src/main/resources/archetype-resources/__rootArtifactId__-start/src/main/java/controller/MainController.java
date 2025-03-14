74ab0058f49b3cddeadad5359c88234d3706f540
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2025-03-14 16:58:25 +0800

    feat: introduce spring-actuator

--- src/main/resources/archetype-resources/__rootArtifactId__-start/src/main/java/controller/MainController.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-start/src/main/java/controller/MainController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,18 +17,5 @@ public class MainController {
     @RequestMapping("/")
     public String index(Model model) {
         return "redirect:/swagger-ui/index.html";
-    }
-
-    @GetMapping("/public/check/live")
-    @ResponseBody
-    public String live() {
-        return "OK";
-    }
-
-    @GetMapping("/public/check/ready")
-    @ResponseBody
-    public String ready() {
-        // TODO: check services statuses
-        return "OK";
     }
 }
